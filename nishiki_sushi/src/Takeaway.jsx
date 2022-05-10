4c50fd5f0f5c10f6a7fb8f83c2892d5b7dbba540
emily <emil7809@gmail.com>
2022-05-10 13:25:53 +0200

    html set up for takeaway

--- nishiki_sushi/src/Takeaway.jsx
+++ nishiki_sushi/src/Takeaway.jsx
@@ -1,11 +1,111 @@
 import React from "react";
+import { IoIosSearch } from "react-icons/io";
+import { BsBasket3Fill } from "react-icons/bs";
+import { TiArrowUnsorted } from "react-icons/ti";
+import { MdOutlineKeyboardBackspace } from "react-icons/md";
 
 function Takeaway() {
-    return (
-        <div>
-            <h1>Take Away</h1>
+  return (
+    <>
+      <div>
+        <h1>Take Away</h1>
+      </div>
+      <div>
+        <p>Sorter Pris</p>
+        <TiArrowUnsorted />
+      </div>
+
+      <div>
+        <IoIosSearch />
+        <input
+          type="text"
+          placeholder="Søg efter sushi eller ingredienser"
+        ></input>
+      </div>
+
+      <div className="kategorier_container">
+        <h2>Kategorier</h2>
+        {/* <template> */}
+        <div
+          className="blue_box"
+          style={{
+            backgroundColor: "#30404F",
+            width: "200px",
+            height: "120px",
+          }}
+        ></div>
+        <img src="" alt="food_img" />
+        <h3>Food Name</h3>
+        {/*  </template> */}
+      </div>
+
+      <div className="result_container">
+        <h2>Results for </h2>
+        {/* <template> */}
+        <span>0</span>
+        <div
+          className="blue_box"
+          style={{
+            backgroundColor: "#30404F",
+            width: "300px",
+            height: "130px",
+          }}
+        ></div>
+        <img src="" alt="food_img" />
+        <h3>Food Name</h3>
+        <p>Food info</p>
+        <p>Pris:</p>
+        <div className="remove_from_basket">
+          <BsBasket3Fill />
+          <p>-</p>
         </div>
-    )
+        <div className="add_to_basket">
+          <BsBasket3Fill style={{ color: "green" }} />
+          <p>+</p>
+        </div>
+        {/*  </template> */}
+      </div>
+
+      <div className="chosen_kategorie_container">
+        <button>
+          {" "}
+          <MdOutlineKeyboardBackspace />
+          Kategorier
+        </button>
+        <div>
+          <IoIosSearch />
+          <input
+            type="text"
+            placeholder="Søg efter sushi eller ingredienser"
+          ></input>
+        </div>
+        <h2>Name of Kategorie </h2>
+        {/* <template> */}
+        <span>0</span>
+        <div
+          className="blue_box"
+          style={{
+            backgroundColor: "#30404F",
+            width: "300px",
+            height: "130px",
+          }}
+        ></div>
+        <img src="" alt="food_img" />
+        <h3>Food Name</h3>
+        <p>Food info</p>
+        <p>Pris:</p>
+        <div className="remove_from_basket">
+          <BsBasket3Fill />
+          <p>-</p>
+        </div>
+        <div className="add_to_basket">
+          <BsBasket3Fill style={{ color: "green" }} />
+          <p>+</p>
+        </div>
+        {/*  </template> */}
+      </div>
+    </>
+  );
 }
 
 export default Takeaway;
