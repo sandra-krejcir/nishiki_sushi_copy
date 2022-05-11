ae180a52b97886441ea220a04c6979a20173bfd5
Rikke Blom <rikk957v@stud.kea.dk>
2022-05-11 09:31:12 +0200

    and an extra for the road

--- nishiki_sushi/src/App.jsx
+++ nishiki_sushi/src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 /* import { useState } from "react"; */
-import Takeaway from "./Takeaway";
-import Kurv from "./Kurv";
-import Oplysninger from "./oplysninger";
-import Betaling from "./Betaling";
-import Bekræftelse from "./Bekræftelse";
+import Takeaway from "./components/Takeaway";
+import Kurv from "./components/Kurv";
+import Oplysninger from "./components/Oplysninger";
+import Betaling from "./components/Betaling";
+import Bekræftelse from "./components/Bekræftelse";
 import LandingPage from "./components/LandingPage";
 
 export default function App() {
