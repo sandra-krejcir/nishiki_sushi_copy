655af22f19084051a6cd79921802dddb34b71c8b
emily <emil7809@gmail.com>
2022-05-12 06:03:03 +0200

    takeaway result done for now

--- nishiki_sushi/src/components/Takeaway.jsx
+++ nishiki_sushi/src/components/Takeaway.jsx
@@ -7,97 +7,92 @@ import { MdOutlineKeyboardBackspace } from "react-icons/md";
 function Takeaway() {
   return (
     <>
-      <div className="takeaway_headline">
-        <h1>
-          <b>Take</b>Away
-        </h1>
-        <div className="sorter_pris">
-          <p>Sorter Pris</p>
-          <TiArrowUnsorted className="hw20_icon" />
+      <div className="takeaway_container">
+        <div className="takeaway_headline">
+          <h1>
+            <b>Take</b>Away
+          </h1>
+          <div className="sorter_pris">
+            <p>Sorter Pris</p>
+            <TiArrowUnsorted className="hw20_icon" />
+          </div>
         </div>
-      </div>
-
-      <div className="search_container">
-        <IoIosSearch className="hw20_icon" />
-        <input
-          type="text"
-          placeholder="Søg efter sushi eller ingredienser"
-        ></input>
-      </div>
-
-      <div className="kategorier_container">
-        <h2>Kategorier</h2>
-
-        <div className="kategorier">
-          <img
-            src="https://www.nishikisushi.dk/media/31wn4jmh/m%C3%A5ned-tilbud-1.jpg"
-            alt="food_img"
-          />
-          <h3>Food Name</h3>
-        </div>
-      </div>
-
-      <div className="result_container">
-        <h2>Results for </h2>
-
-        <span>0</span>
-        <div
-          className="blue_box"
-          style={{
-            backgroundColor: "#30404F",
-            width: "300px",
-            height: "130px",
-          }}
-        ></div>
-        <img src="" alt="food_img" />
-        <h3>Food Name</h3>
-        <p>Food info</p>
-        <p>Pris:</p>
-        <div className="remove_from_basket">
-          <BsBasket3Fill />
-          <p>-</p>
-        </div>
-        <div className="add_to_basket">
-          <BsBasket3Fill c />
-          <p>+</p>
-        </div>
-      </div>
-
-      <div className="chosen_kategorie_container">
-        <button className="secondaryBtn">
-          {" "}
-          <MdOutlineKeyboardBackspace />
-          Kategorier
-        </button>
-        <div>
-          <IoIosSearch />
+        <div className="search_container">
+          <IoIosSearch className="hw20_icon" />
           <input
             type="text"
             placeholder="Søg efter sushi eller ingredienser"
           ></input>
         </div>
-        <h2>Name of Kategorie </h2>
-
-        <span>0</span>
-        <div
-          className="blue_box"
-          style={{
-            backgroundColor: "#30404F",
-            width: "300px",
-            height: "130px",
-          }}
-        ></div>
-        <img src="" alt="food_img" />
-        <h3>Food Name</h3>
-        <p>Food info</p>
-        <p>Pris:</p>
-        <div className="remove_from_basket">
-          <BsBasket3Fill />
-          <p>-</p>
+        <div className="kategorier_container">
+          <h2>Kategorier</h2>
+          <div className="kategorier">
+            <img
+              src="https://www.nishikisushi.dk/media/31wn4jmh/m%C3%A5ned-tilbud-1.jpg"
+              alt="food_img"
+            />
+            <h3>Food Name</h3>
+          </div>
         </div>
-        <div className="add_to_basket">
-          <BsBasket3Fill style={{ color: "green" }} />
-          <p>+</p>
+        <h2>
+          {}1 Resultater for "{}"
+        </h2>
+        <div className="result_container">
+          <div className="item">
+            <div className="in_basket_number_container">
+              <p className="in_basket_number">0</p>
+            </div>
+            <img
+              src="https://images-global.nhst.tech/image/bUpscFAvOWZnVnVIQ283TlAyeFI0WFMwZ0RiVjRucGhyek52d0pCdUJ3OD0=/nhst/binary/66e1fa282d12f429459b8b83628bb8b4?image_version=640"
+              alt="food_img"
+            />
+            <h4>Food Name</h4>
+            <p>Food info ærguaobgro oirae opaiuhr opuerh opeuarhol</p>
+            <p>Pris:</p>
+            <div className="basket_icons">
+              <div className="remove_from_basket">
+                <BsBasket3Fill className="hw40_icon" />
+              </div>
+              <div className="add_to_basket">
+                <BsBasket3Fill className="hw40_icon" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="chosen_kategorie_container">
+          <button className="secondaryBtn">
+            <MdOutlineKeyboardBackspace />
+            Kategorier
+          </button>
+          <div>
+            <IoIosSearch />
+            <input
+              type="text"
+              placeholder="Søg efter sushi eller ingredienser"
+            ></input>
+          </div>
+          <h2>Name of Kategorie </h2>
+          <span>0</span>
+          <div
+            className="blue_box"
+            style={{
+              backgroundColor: "#30404F",
+              width: "300px",
+              height: "130px",
+            }}
+          ></div>
+          <img src="" alt="food_img" />
+          <h3>Food Name</h3>
+          <p>Food info</p>
+          <p>Pris:</p>
+          <div className="remove_from_basket">
+            <BsBasket3Fill />
+            <p>-</p>
+          </div>
+          <div className="add_to_basket">
+            <BsBasket3Fill style={{ color: "green" }} />
+            <p>+</p>
+          </div>
         </div>
       </div>
     </>
