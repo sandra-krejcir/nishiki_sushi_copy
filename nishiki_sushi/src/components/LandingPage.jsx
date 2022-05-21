bd71236eaa25769e69a8b385c48ff3526267ba04
Sandra Krejcir <krejcir.sandra@gmail.com>
2022-05-21 16:36:58 +0200

    added scss for mobile navigation

--- nishiki_sushi/src/components/LandingPage.jsx
+++ nishiki_sushi/src/components/LandingPage.jsx
@@ -2,13 +2,19 @@ import NewOffersSection from "./NewOffersSection";
 import VisitUsSection from "./VisitUsSection";
 import CateringAndEventsSection from "./CateringAndEventsSection";
 import BurgerMenu from "./BurgerMenu";
+import MobileNav from "./MobileNavBar";
 
 export default function LandingPage() {
   return (
     <>
       <BurgerMenu page={"home"} />
+      <MobileNav page={"home"} />
       <section className="thumbnail_images">
-        <img src="../icons/logo.svg" alt="The logo icon." className="header_logo" />
+        <img
+          src="../icons/logo.svg"
+          alt="The logo icon."
+          className="header_logo"
+        />
         <a href="" className="thumbnail_img restaurant_thumbnail">
           <h4 className="restaurant_title">
             <b>BESØG RESTAURANTEN</b>
