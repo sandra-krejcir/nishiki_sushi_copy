f64288b3195611c695b276747e52e0e1f234a70f
emily <emil7809@gmail.com>
2022-05-23 11:11:00 +0200

    pæpåkihkc

--- nishiki_sushi/src/components/Takeaway.jsx
+++ nishiki_sushi/src/components/Takeaway.jsx
@@ -63,7 +63,7 @@ function Takeaway() {
             placeholder="Søg efter sushi eller ingredienser"
             onChange={(event) => {
               setsearchTerm(event.target.value);
-              if (searchTerm.length < 1) {
+              if (searchTerm.length < 2) {
                 onChangeSearch();
               } else if (searchTerm.length === 0) {
               }
@@ -71,7 +71,8 @@ function Takeaway() {
             /*  onClick={onChangeSearch} */
           ></input>
         </div>
-        <div className={`hidden ${visible ? "show" : ""} kategorier_container`}>
+        {/*  <div className={`hidden ${visible ? "show" : ""} kategorier_container`}> */}
+        {/* <div className="kategorier_container">
           <h2>Kategorier</h2>
           <div className="kategorier">
             <div className="kategorie manedens_tilbud">
@@ -159,26 +160,120 @@ function Takeaway() {
         </div>
         <h2>
           {}1 Resultater for "{}"
-        </h2>
+        </h2> */}
         <div className="result_container">
           {sushiData
             .filter((val) => {
               if (searchTerm === "") {
-                return null;
-                /*  return val; */
+                return (
+                  <>
+                    <div className="kategorier_container">
+                      <h2>Kategorier</h2>
+                      <div className="kategorier">
+                        <div className="kategorie manedens_tilbud">
+                          <img
+                            src="../../img/kategorier_billeder/manedenstilbud.png"
+                            alt="månedens_tilbud"
+                          />
+                          <h3>Månedens tilbud</h3>
+                        </div>
+                        <div className="kategorie sushi_menu">
+                          <img
+                            src="../../img/kategorier_billeder/sushimenu.png"
+                            alt="sushi_menu"
+                          />
+                          <h3>Sushi Menu</h3>
+                        </div>
+                        <div className="kategorie forret">
+                          <img
+                            src="../../img/kategorier_billeder/forret.png"
+                            alt="forret"
+                          />
+                          <h3>Forret</h3>
+                        </div>
+                        <div className="kategorie yakitori_sticks">
+                          <img
+                            src="../../img/kategorier_billeder/yakitori_sticks.png"
+                            alt="yakitori_sticks"
+                          />
+                          <h3>Yakitori Sticks</h3>
+                        </div>
+                        <div className="kategorie nigiri">
+                          <img
+                            src="../../img/kategorier_billeder/nigiri.png"
+                            alt="nigiri"
+                          />
+                          <h3>Nigiri 2stk</h3>
+                        </div>
+                        <div className="kategorie hosomaki">
+                          <img
+                            src="../../img/kategorier_billeder/hosomaki.png"
+                            alt="hosomaki"
+                          />
+                          <h3>Hosomaki 8stk</h3>
+                        </div>
+                        <div className="kategorie futomaki">
+                          <img
+                            src="../../img/kategorier_billeder/futomaki.png"
+                            alt="futomaki"
+                          />
+                          <h3>Futomaki 5stk</h3>
+                        </div>
+                        <div className="kategorie sashimi">
+                          <img
+                            src="../../img/kategorier_billeder/sashimi.png"
+                            alt="sashimi"
+                          />
+                          <h3>Sashimi</h3>
+                        </div>
+                        <div className="kategorie rispapir">
+                          <img
+                            src="../../img/kategorier_billeder/rispapir.png"
+                            alt="rispapir"
+                          />
+                          <h3>Rispapir 6stk</h3>
+                        </div>
+                        <div className="kategorie uramaki">
+                          <img
+                            src="../../img/kategorier_billeder/uramaki.png"
+                            alt="uramaki"
+                          />
+                          <h3>Uramaki 8stk</h3>
+                        </div>
+                        <div className="kategorie kaburimaki">
+                          <img
+                            src="../../img/kategorier_billeder/kaburimaki.png"
+                            alt="kaburimaki"
+                          />
+                          <h3>Kaburimaki 8stk</h3>
+                        </div>
+                        <div className="kategorie dip">
+                          <img
+                            src="../../img/kategorier_billeder/dip.png"
+                            alt="dip"
+                          />
+                          <h3>Dip</h3>
+                        </div>
+                      </div>
+                    </div>
+                    <h2>
+                      {}1 Resultater for "{}"
+                    </h2>
+                  </>
+                );
               } else if (
                 val.name.toLowerCase().includes(searchTerm.toLowerCase())
               ) {
                 /*  onChangeSearch(); */
                 return val;
               } else if (
-                val.category.toLowerCase().includes(searchTerm.toLowerCase())
+                val.ingrediant_list
+                  .toLowerCase()
+                  .includes(searchTerm.toLowerCase())
               ) {
                 return val;
               } else if (searchTerm === "alle") {
                 return val;
-              } else if (searchTerm.length === 0) {
-                onChangeSearch();
               }
             })
             .map((val, key) => {
