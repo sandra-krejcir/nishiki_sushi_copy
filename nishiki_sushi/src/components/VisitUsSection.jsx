cc2e669320c5a04226a125a2198f33d4e6113100
emily <emil7809@gmail.com>
2022-05-30 13:17:44 +0200

    addded weekday class

--- nishiki_sushi/src/components/VisitUsSection.jsx
+++ nishiki_sushi/src/components/VisitUsSection.jsx
@@ -1,6 +1,9 @@
 import { NavLink } from "react-router-dom";
 
 export default function VisitUsSection() {
+  const today = new Date().getDay();
+  console.log("weekday" + today);
+
   return (
     <section className="side_row">
       <div className="side_picture"></div>
@@ -13,31 +16,31 @@ export default function VisitUsSection() {
             <div className="times">
               <h3>Åbningtider</h3>
               <div className="line"></div>
-              <div className="time_row">
+              <div className="time_row weekday1">
                 <p>Mondag</p>
                 <p>15:00 - 22:00</p>
               </div>
-              <div className="time_row">
+              <div className="time_row weekday2">
                 <p>Tirsdag</p>
                 <p>15:00 - 22:00</p>
               </div>
-              <div className="time_row">
+              <div className="time_row weekday3">
                 <p>Onsdag</p>
                 <p>15:00 - 22:00</p>
               </div>
-              <div className="time_row">
+              <div className="time_row weekday4">
                 <p>Torsdag</p>
                 <p>15:00 - 22:00</p>
               </div>
-              <div className="time_row">
+              <div className="time_row weekday5">
                 <p>Fridag</p>
                 <p>13:00 - 22:00</p>
               </div>
-              <div className="time_row">
+              <div className="time_row weekday6">
                 <p>Lørdag</p>
                 <p>13:00 - 22:00</p>
               </div>
-              <div className="time_row">
+              <div className="time_row weekday7">
                 <p>Sondag</p>
                 <p>13:00 - 22:00</p>
               </div>
