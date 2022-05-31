3313a3378506fa553a5104f2df989a44de8fc014
emily <emil7809@gmail.com>
2022-05-31 09:20:57 +0200

    a error gone

--- nishiki_sushi/src/components/LandingPage.jsx
+++ nishiki_sushi/src/components/LandingPage.jsx
@@ -14,40 +14,53 @@ export default function LandingPage(props) {
   return (
     <>
       <AnimatedPage>
-        {screenSize.width > 799 && <BurgerMenu cartContents={props.cartContents} page={"home"} />}
-        {screenSize.width < 799 && <MobileNav cartContents={props.cartContents} page={"home"} />}
+        {screenSize.width > 799 && (
+          <BurgerMenu cartContents={props.cartContents} page={"home"} />
+        )}
+        {screenSize.width < 799 && (
+          <MobileNav cartContents={props.cartContents} page={"home"} />
+        )}
         <section className="thumbnail_images">
-          <img style={{ zIndex: "99" }} src="../icons/logo.svg" alt="The logo icon." className="header_logo header_logo_landing_page" />
+          <img
+            style={{ zIndex: "99" }}
+            src="../icons/logo.svg"
+            alt="The logo icon."
+            className="header_logo header_logo_landing_page"
+          />
 
-          <motion.a
-            href=""
+          <motion.div
             className="thumbnail_img restaurant_thumbnail"
             whileHover={{ scale: 1.03 }}
             whileTap={{
               scale: 0.98,
             }}
           >
-            <NavLink to="/restaurant" className="thumbnail_img restaurant_thumbnail">
+            <NavLink
+              to="/restaurant"
+              className="thumbnail_img restaurant_thumbnail"
+            >
               <h4 className="restaurant_title">
                 <b>BESØG RESTAURANTEN</b>
               </h4>
             </NavLink>
-          </motion.a>
+          </motion.div>
 
-          <motion.a
-            href=""
+          <motion.div
             className="thumbnail_img takeaway_thumbnail"
             whileHover={{ scale: 1.03 }}
             whileTap={{
               scale: 0.98,
             }}
           >
-            <NavLink to="/takeaway" className="thumbnail_img takeaway_thumbnail">
+            <NavLink
+              to="/takeaway"
+              className="thumbnail_img takeaway_thumbnail"
+            >
               <h4 className="takeaway_title">
                 <b>TAKEAWAY</b>
               </h4>
             </NavLink>
-          </motion.a>
+          </motion.div>
         </section>
         <NewOffersSection />
         <VisitUsSection />
