582f4bf8a8c545db56c8e8b7a4537f1f4a7391fa
Rikke Blom <rikk957v@stud.kea.dk>
2022-06-09 20:56:48 +0200

    fix: $ ends pattern in validation

--- src/components/ValidateBetalingForm.jsx
+++ src/components/ValidateBetalingForm.jsx
@@ -2,8 +2,8 @@ export default function validateInfoBetaling(values) {
   let errors = {};
   if (!values.betaling_kortnummer.trim()) {
     errors.betaling_kortnummer = "kortnummer påkrævet";
-  } else if (!/^[0-9]{4}[ ][0-9]{4}[ ][0-9]{4}[ ][0-9]{4}/.test(values.betaling_kortnummer.trim())) {
-    if (!/^[0-9]{16}/.test(values.betaling_kortnummer.trim())) {
+  } else if (!/^[0-9]{4}[ ][0-9]{4}[ ][0-9]{4}[ ][0-9]{4}$/.test(values.betaling_kortnummer.trim())) {
+    if (!/^[0-9]{16}$/.test(values.betaling_kortnummer.trim())) {
       errors.betaling_kortnummer = "Fejl i kortnummer";
     }
   }
